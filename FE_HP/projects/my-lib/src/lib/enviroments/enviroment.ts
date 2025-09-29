5ba2db7a86ca9303afacc56d6bf510bc4069189c
Duy Hoàng <duypo03cbvn@gmail.com>
2025-09-29 18:35:46 +0700

    Merge new code

--- FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
+++ FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1',
-  wsUrl: 'http://localhost:5000',
-  // apiUrl: 'http://192.168.0.30:5000/api/v1',
-  // wsUrl: 'http://192.168.0.30:5000',
+  // apiUrl: 'http://localhost:5000/api/v1',
+  // wsUrl: 'http://localhost:5000',
+  apiUrl: 'http://192.168.0.30:5000/api/v1',
+  wsUrl: 'http://192.168.0.30:5000',
   security: {
     enableLogging: true,
     tokenExpiry: 3600,
