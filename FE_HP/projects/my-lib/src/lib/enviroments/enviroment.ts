f95e09e5bfac237f9241deb84f562bf651385c5d
Duy Hoàng <duypo03cbvn@gmail.com>
2025-09-30 15:04:29 +0700

    Update domain

--- FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
+++ FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000/api/v1',
   // wsUrl: 'http://localhost:5000',
-  apiUrl: 'demo-nhp-api.huce.edu.vn',
-  wsUrl: 'demo-nhp-api.huce.edu.vn',
+  apiUrl: 'https://demo-nhp.huce.edu.vn',
+  wsUrl: 'https://demo-nhp.huce.edu.vn',
   security: {
     enableLogging: true,
     tokenExpiry: 3600,
