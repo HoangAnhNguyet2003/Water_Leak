ecad6a98594f7943be9bef1f69d3025e60a5bbc9
Duy Hoàng <duypo03cbvn@gmail.com>
2025-10-01 16:29:21 +0700

    Limit popup

--- FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
+++ FE_HP/projects/my-lib/src/lib/enviroments/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1',
-  wsUrl: 'http://localhost:5000',
-  // apiUrl: 'https://demo-nhp-api.huce.edu.vn/api/v1',
-  // wsUrl: 'https://demo-nhp-api.huce.edu.vn',
+  // apiUrl: 'http://localhost:5000/api/v1',
+  // wsUrl: 'http://localhost:5000',
+  apiUrl: 'https://demo-nhp-api.huce.edu.vn/api/v1',
+  wsUrl: 'https://demo-nhp-api.huce.edu.vn',
 };
